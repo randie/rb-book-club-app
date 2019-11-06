fc974459324f6a3d6a8f2f7943510fa088757945
Randie B <randie.bemis@gmail.com>
2019-11-05 16:33:58 -0800

    clean up

--- src/templates/book-details.js
+++ src/templates/book-details.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 const BookDetails = ({ pageContext: book }) => (
-  <pre>{JSON.stringify(book, null, 2)}</pre>
+  <Layout>
+    <pre>{JSON.stringify(book, null, 2)}</pre>
+  </Layout>
 );
 
 export default BookDetails;
