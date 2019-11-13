4d1c35eaab1ef07b096bfb5a89ba53fc678e257c
Randie B <randie.bemis@gmail.com>
2019-11-12 16:20:10 -0800

    refactor index.js to use useStaticQuery  hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 import BookList from '../components/book-list';
 
-export const query = graphql`
-  {
-    allBook {
-      nodes {
-        id
-        title
-        author {
-          name
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      allBook {
+        nodes {
+          id
+          title
+          author {
+            name
+          }
+          summary
         }
-        summary
       }
     }
-  }
-`;
+  `);
 
-const Index = ({ data }) => (
-  <Layout>
-    <BookList books={data.allBook.nodes} />
-  </Layout>
-);
-
-export default Index;
+  return (
+    <Layout>
+      <BookList books={data.allBook.nodes} />
+    </Layout>
+  );
+};
