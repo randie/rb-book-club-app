782b00ab53d6239f304ebe6134f7ac8c7258b44f
Randie B <randie.bemis@gmail.com>
2019-11-13 00:09:45 -0800

    tweak Book markup and styles

--- src/components/book.js
+++ src/components/book.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const BookContainer = styled.section`
+const BookContainer = styled.div`
   display: flex;
   border: 2px solid #ddd;
   border-radius: 8px;
   padding: 1rem;
-  & + section {
+  & + div {
     margin-top: 0.5rem;
   }
+  header {
+    margin-bottom: 1.2rem;
+  }
   h2 {
-    small {
-      font-weight: normal;
-      font-size: 1.2rem;
+    margin-bottom: 0.4rem;
+    & + div {
+      font: 1.1rem Arial, Helvetica, sans-serif;
     }
   }
   img {
     display: block;
     max-width: 200px;
-    & + div {
-      margin-left: 1rem;
-    }
+    max-height: 300px;
+    margin-right: 1rem;
   }
 `;
 
@@ -28,11 +30,10 @@ const Book = ({ title, summary, author, imageUrl, children }) => (
   <BookContainer>
     <img src={imageUrl} alt="book cover" />
     <div>
-      <h2>
-        {title}
-        <br />
-        <small>{author}</small>
-      </h2>
+      <header>
+        <h2>{title}</h2>
+        <div>by {author}</div>
+      </header>
       <p>{summary}</p>
       {children}
     </div>
