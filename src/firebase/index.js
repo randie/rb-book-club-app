d0df632d536a2dc23022a1bc55c476a0a7a1a546
Randie B <randie.bemis@gmail.com>
2019-11-17 20:50:14 -0800

    rename loadFirebaseDependencies.js to load-firebase-dependencies.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
 import getFirebase from './firebase';
 import FirebaseContext from './context';
-import loadFirebaseDependencies from './loadFirebaseDependencies';
+import loadFirebaseDependencies from './load-firebase-dependencies';
 
 export default getFirebase;
 export { FirebaseContext, loadFirebaseDependencies };
