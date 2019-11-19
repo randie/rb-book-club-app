76e9986c0dfcdcc9748ac37ec344a87eba09fe96
Randie B <randie.bemis@gmail.com>
2019-11-18 18:07:07 -0800

    flesh out register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,3 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FirebaseContext, useAuth } from '../firebase';
+import { Form, Input, Button } from '../components';
 
-export default () => <div>Register Page</div>;
+const Register = () => {
+  const { firebase } = useAuth(FirebaseContext);
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  function handleInputChange(event) {
+    event.persist();
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //firebase.register({ email, password, displayName: 'foobar' }).then(() => navigate('/')); // TODO: .catch()
+  }
+
+  const { email, password, confirmPassword } = formValues;
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        name="email"
+        value={email}
+        placeholder="Email"
+        required
+        onChange={handleInputChange}
+      />
+      <Input
+        type="password"
+        name="password"
+        value={password}
+        placeholder="Password"
+        required
+        minLength={4}
+        onChange={handleInputChange}
+      />
+      <Input
+        type="password"
+        name="confirmPassword"
+        value={confirmPassword}
+        placeholder="Confirm Password"
+        required
+        minLength={4}
+        onChange={handleInputChange}
+      />
+      <Button type="submit" block>
+        Register
+      </Button>
+
+      {/* TODO: remove, for debugging only */}
+      {(email || password || confirmPassword) && <pre>{JSON.stringify(formValues, null, 2)}</pre>}
+    </Form>
+  );
+};
+
+export default Register;
