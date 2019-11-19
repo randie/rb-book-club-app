aeadd0fca7dc3604c93607d03ca6dc36a0c81dee
Randie B <randie.bemis@gmail.com>
2019-11-18 19:49:54 -0800

    add login form validation and handle login errors

---
+++ src/components/message.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Message = styled.div`
+  color: ${props => (props.error ? 'red' : '#666')};
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 0.9rem;
+  margin-top: 0.5rem;
+`;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,40 +1,48 @@
 import React, { useState, useContext } from 'react';
 import { navigate } from 'gatsby';
 import { FirebaseContext } from '../firebase';
-import { Form, Input, Button } from '../components';
+import { Form, Input, Button, Message } from '../components';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const { firebase } = useContext(FirebaseContext);
 
   function handleEmailChange(event) {
     event.persist();
     setEmail(event.target.value);
+    setErrorMessage('');
   }
 
   function handlePasswordChange(event) {
     event.persist();
     setPassword(event.target.value);
+    setErrorMessage('');
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    firebase.login({ email, password }).then(() => navigate('/')); // TODO: .catch()
+    firebase
+      .login({ email, password })
+      .then(() => navigate('/'))
+      .catch(error => setErrorMessage(error.message));
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+      <Input type="email" placeholder="Email" value={email} onChange={handleEmailChange} required />
       <Input
         type="password"
         placeholder="Password"
         value={password}
         onChange={handlePasswordChange}
+        required
       />
       <Button type="submit" block>
         Login
       </Button>
+      {errorMessage && <Message error>{errorMessage}</Message>}
     </Form>
   );
 };
